a13b0fca7c1fec16d42a41e5812f59603e06150d
Rafael Menezes <menezes.inbox@gmail.com>
2023-09-28 10:18:37 -0300

    fix: config

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,6 @@ import 'reflect-metadata';
 import '../typeorm/connection';
 import { app } from './app';
 
-const PORT = 4444;
+const PORT = 8080;
 
 app.listen(PORT, () => console.log('🚀 Server is running on port', PORT));
